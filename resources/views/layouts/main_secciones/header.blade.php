7826ac5cd45f9f345e4f378283f05899fd4a5638
matias <matias@webmedia-arg.com.ar>
2023-06-06 10:12:19 -0300

    commit header.blade

--- resources/views/layouts/main_secciones/header.blade.php
+++ resources/views/layouts/main_secciones/header.blade.php
@@ -153,13 +153,13 @@
             <li class="nav-item dropdown no-caret dropdown-user me-3 me-lg-4">                                                                                                                                                                                                                                             
                 <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownUserImage" href="javascript:void(0);" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img class="img-fluid" src="{{asset(Storage::disk("usuarios")->url(Auth::user()->foto))}}" /></a>
                 <div class="dropdown-menu dropdown-menu-end border-0 shadow animated--fade-in-up" aria-labelledby="navbarDropdownUserImage">
-                    <h6 class="dropdown-header d-flex align-items-center">
-                        <img class="dropdown-user-img" src="{{Storage::disk("usuarios")->url(Auth::user()->foto)}}" />
+                    <!-- <h6 class="dropdown-header d-flex align-items-center">
+                        <img class="dropdown-user-img" src="{{Storage::disk("usuarios")->url(Auth::user()->foto)}}" /> 
                         <div class="dropdown-user-details">
                             <div class="dropdown-user-details-name">{{Auth()->user()->name}}</div>
                             <div class="dropdown-user-details-email">{{Auth()->user()->email}}</div>
                         </div>
-                    </h6>
+                    </h6> -->
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="#!">
                         <div class="dropdown-item-icon"><i data-feather="settings"></i></div>
