f7cf3cdb7e6bc6fd53d6bd4bbec1bb09013f1f3a
= <barriosjc@yahoo.com.ar>
2023-10-29 19:25:36 -0300

    actualizaciones nuevas

--- app/Models/Periodo.php
+++ app/Models/Periodo.php
@@ -54,7 +54,7 @@ class Periodo extends Model
     {
         $resu = '';
         if (!empty($value)) {
-            $resu = date('m/d/Y', strtotime($value));
+            $resu = date('d/m/Y', strtotime($value));
         }
 
         return $resu;
